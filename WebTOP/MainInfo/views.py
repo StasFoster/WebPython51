e9887651d14331d59ac696f3f62f3f6ceecb4ff5
StasFoster <111638493+StasFoster@users.noreply.github.com>
2025-10-08 20:01:02 +0400

    last

--- WebTOP/MainInfo/views.py
+++ WebTOP/MainInfo/views.py
@@ -2,19 +2,23 @@ from django.shortcuts import render, redirect
 from . import models
 from . import forms
 from django.contrib.auth import login
+from django.views.decorators.cache import never_cache
+#
+# def temp(func):
+#     def wrapper():
 
 # Create your views here.
+@never_cache
 def index(request):
-    # if request.method == "POST":
-    #     form = forms.MainInfoForm(request.POST)
-    #     if form.is_valid():
-    #         user = form.save()
-    #         login(request, user)
-    #         return redirect("index")
-    #
-    # else:
-    #     form = forms.MainInfoForm()
-    form = forms.EventForm()
+    if request.method == "POST":
+        form = forms.MainInfoForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("home")
+
+    else:
+        form = forms.MainInfoForm()
     data  = {
         'form': form,
     }
@@ -26,6 +30,6 @@ def home(request):
     data = {}
     if users:
         data = {
-            'list_users': [i.username for i in users],
+            'users': users,
         }
     return render(request, 'MainInfo/home.html', data)
