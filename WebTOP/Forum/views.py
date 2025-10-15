bdbf0f305d8486174e22399fa3c191f0fe1b2986
StasFoster <111638493+StasFoster@users.noreply.github.com>
2025-10-15 13:11:07 +0400

    last4

---
+++ WebTOP/Forum/views.py
@@ -0,0 +1,15 @@
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.views.decorators.cache import never_cache
+from django.contrib.auth import logout
+
+# Create your views here.
+@never_cache
+def forum(request):
+    data = {"is_signin": False}
+    print("out")
+    if request.user.is_authenticated:
+        data["is_signin"] = True
+        print("inside forum")
+    return render(request, 'Forum/index.html', data)
+
