c25323ce2ac97b06886de9ef44e6425f414d2257
PhantomRf64 <106621228+PhantomRf64@users.noreply.github.com>
2025-10-15 12:58:49 +0400

    LAst 5

--- WebTOP/Forum/urls.py
+++ WebTOP/Forum/urls.py
@@ -18,11 +18,11 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import RedirectView
+from django.views.generic.base import RedirectView
 from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('forum/', views.forum , name='forum' ),
-
+    path('forum/', views.forum, name='forum'),
+    path('', RedirectView.as_view(pattern_name='forum')),
 ]
